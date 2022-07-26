0fb7f35616f4bbce806c38191c9f286c4f4fc88d
she11fish <trolltrolltrolllollollol@outlook.com>
2022-07-25 23:55:17 -0400

    uploading files

--- content.js
+++ content.js
@@ -24,9 +24,11 @@ function initialize_reply_observers(open_reply) {
     for (let reply_change of reply_changes) open_reply.observe(reply_change, config)
 }
 
-function check_spam(user_comments, comment) {
-    for (let user_comment of user_comments) 
-        if (user_comment === comment) return true
+function check_spam(comment, user_comments, comment_box, comment_boxes) {
+    for (const [index , user_comment] of user_comments.entries()) {
+        current_box = comment_boxes[index]
+        if (user_comment === comment && current_box !== comment_box) return true
+    }
     return false
 }
 
@@ -69,7 +71,7 @@ function main() {
             if (check_duplicate_box) { i++; continue }
             reply_box.setAttribute("check","true")
             let img = reply_box.querySelector('img')
-            let comment = reply_box.querySelector('ytd-expander yt-formatted-string').innerText
+            let reply = reply_box.querySelector('ytd-expander yt-formatted-string').innerText
             let regex = /<a|<\/a>/g
             let comment_html = reply_box.querySelector('ytd-expander yt-formatted-string').innerHTML
             let a_tag = comment_html.match(regex)
@@ -83,15 +85,15 @@ function main() {
                 continue
             }
             if (!reply_data.hasOwnProperty(channel_name)) {
-                reply_data[channel_name] = {comment_boxes: [reply_box], comments: [comment], img: img} 
+                reply_data[channel_name] = { reply_boxes: [reply_box], replies: [reply], img: img } 
             } else {
-                var comments = reply_data[channel_name].comments
-                var comment_boxes = reply_data[channel_name].comment_boxes
-                if (check_spam(comments, comment)) {
+                var replies = reply_data[channel_name].replies
+                var reply_boxes = reply_data[channel_name].reply_boxes
+                if (check_spam(reply, replies, reply_box, reply_boxes)) {
                     blacklist.push(channel_name)
-                    comments.push(comment)
-                    comment_boxes.push(reply_box)
-                    for (let comment_box of comment_boxes) { comment_box.remove(); }
+                    replies.push(reply)
+                    reply_boxes.push(reply_box)
+                    for (let comment_box of reply_boxes) { comment_box.remove(); }
                     delete reply_data[channel_name]
                 }
             } 
@@ -121,61 +123,60 @@ function main() {
     
     const end_page = new IntersectionObserver(() => {
         console.log('end of page')
-        setTimeout(() => { 
-            if (!state) {
-                end_page.disconnect()
-                console.log('end page disconnected')
-                return
-            }
-            let comment_box = document.querySelector('#primary ytd-item-section-renderer > #contents > ytd-continuation-item-renderer')
-            let i = 19
-            while (i >= 0) {
-                if (!comment_box || !comment_box.previousSibling) { i--; continue }
-                comment_box = comment_box.previousSibling
-                if (comment_box.nodeName === 'ytd-continuation-item-renderer'.toUpperCase()) return
-                console.log(comment_box.parentElement.children)
-                let spaces = /\s?(^\s+|\s+$)/g
-                let channel_name = comment_box.querySelector('span').innerText.replace(spaces, '')
-                if (blacklist.includes(channel_name)) {
-                    comment_box.style = "display: none !important"
-                    continue
-                } 
-                let check_duplicate_box = comment_box.getAttribute('check')
-                if (check_duplicate_box) { i--; continue }
-                console.log(comment_box)
-                let img = comment_box.querySelector('img')
-                let comment = comment_box.querySelector('ytd-expander yt-formatted-string').innerText
-                comment_box.setAttribute("check", "true")
-                if (!comment_data.hasOwnProperty(channel_name)) {
-                    comment_data[channel_name] = { comment_boxes: [comment_box], comments: [comment], img: img }
-                } else {
-                    var comments = comment_data[channel_name].comments
-                    var comment_boxes = comment_data[channel_name].comment_boxes
-                    if (check_spam(comments, comment)) {
-                        blacklist.push(channel_name)
-                        comments.push(comment)
-                        comment_boxes.push(comment_box)
-                        for (let comment_box of comment_boxes) comment_box.style = "display: none !important"
-                        delete comment_data[channel_name]
-                        continue
-                    } 
-                } 
-                let regex = /<a|<\/a>/g
-                let comment_html = comment_box.querySelector('ytd-expander yt-formatted-string').innerHTML
-                let a_tag = comment_html.match(regex)
-                let tagging_username = comment_html.match(/@/g)
-                if (a_tag) a_tag = a_tag.join("")
-                if (tagging_username) tagging_username = tagging_username.join("")
-                if (a_tag === '<a</a>' && tagging_username !== "@") {
+        if (!state) {
+            end_page.disconnect()
+            console.log('end page disconnected')
+            return
+        }
+        let comment_box = document.querySelector('#primary ytd-item-section-renderer > #contents > ytd-continuation-item-renderer')
+        let i = 19
+        while (i >= 0) {
+            if (!comment_box || !comment_box.previousSibling) { i--; continue }
+            comment_box = comment_box.previousSibling
+            if (comment_box.nodeName === 'ytd-continuation-item-renderer'.toUpperCase()) return
+            console.log(comment_box.parentElement.children)
+            let spaces = /\s?(^\s+|\s+$)/g
+            let channel_name = comment_box.querySelector('span').innerText.replace(spaces, '')
+            if (blacklist.includes(channel_name)) {
+                comment_box.style = "display: none !important"
+                continue
+            } 
+            let check_duplicate_box = comment_box.getAttribute('check')
+            if (check_duplicate_box) { i--; continue }
+            console.log(comment_box)
+            let img = comment_box.querySelector('img')
+            let comment = comment_box.querySelector('ytd-expander yt-formatted-string').innerText
+            comment_box.setAttribute("check", "true")
+            if (!comment_data.hasOwnProperty(channel_name)) {
+                comment_data[channel_name] = { comment_boxes: [comment_box], comments: [comment], img: img }
+            } else {
+                var comments = comment_data[channel_name].comments
+                var comment_boxes = comment_data[channel_name].comment_boxes
+                if (check_spam(comment, comments, comment_box, comment_boxes)) {
+                    console.log(comment_data[channel_name])
                     blacklist.push(channel_name)
-                    comment_box.style="display: none !important"
+                    comments.push(comment)
+                    comment_boxes.push(comment_box)
+                    for (let comment_box of comment_boxes) comment_box.style = "display: none !important"
+                    delete comment_data[channel_name]
                     continue
-                }
-                console.log(comment_data)
-                console.log(blacklist)
-                i--;
+                } 
+            } 
+            let regex = /<a|<\/a>/g
+            let comment_html = comment_box.querySelector('ytd-expander yt-formatted-string').innerHTML
+            let a_tag = comment_html.match(regex)
+            let tagging_username = comment_html.match(/@/g)
+            if (a_tag) a_tag = a_tag.join("")
+            if (tagging_username) tagging_username = tagging_username.join("")
+            if (a_tag === '<a</a>' && tagging_username !== "@") {
+                blacklist.push(channel_name)
+                comment_box.style="display: none !important"
+                continue
             }
-        }, 0)
+            console.log(comment_data)
+            console.log(blacklist)
+            i--;
+        }
     })
     console.log('in main', state)
 }
